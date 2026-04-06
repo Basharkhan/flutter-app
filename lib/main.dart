7e89daf01a6848dd14d6100b363b2966f81fcf52
BasharKhan <basharkhan71@gmail.com>
2026-04-07 00:02:34 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String title = "Hello";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),   
+          centerTitle: true,       
+        ),
+      ),
+    );
+  }
+}
+
+// begin from 1 hour and 8 minutes
