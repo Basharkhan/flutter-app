fa235ec58b93f45b81765cc4c69b605247b9f19a
BasharKhan <basharkhan71@gmail.com>
2026-04-08 23:24:02 +0600

    driving

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,60 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),   
-          centerTitle: true,       
-        ),
-      ),
+      home: MyHomePage()
     );
   }
 }
 
-// begin from 1 hour and 8 minutes
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter App"),   
+          centerTitle: true,       
+        ),
+        body: currentIndex == 0 ? Center(
+          child: Text(
+            "Home Page",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ) : Center(
+          child: Text(
+            "Profile Page",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.person),
+              label: "Profile",
+            ),           
+          ],
+          selectedIndex: currentIndex,
+          onDestinationSelected: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ),
+      );
+  }
+}
+      
+// begin from 1 hour and 13 minutes in the video
