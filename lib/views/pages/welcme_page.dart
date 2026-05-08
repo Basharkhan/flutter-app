e0c7c5c9ed50feca12b5f855605e564324d74c3d
BasharKhan <basharkhan71@gmail.com>
2026-05-09 01:02:10 +0600

    welcome page

--- lib/views/pages/welcme_page.dart
+++ lib/views/pages/welcme_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/views/wodget_tree.dart';
+import 'package:lottie/lottie.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -12,16 +13,49 @@ class WelcomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset('assets/images/bg.jpg')
+            Lottie.asset('assets/lotties/welcome.json'),
+            FittedBox(
+              child: Text(
+                "Flutter Map",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50.0,
+                  letterSpacing: 50.0,
+                ),
+              ),
+            ),           
+            SizedBox(height: 20.0),
+            FilledButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const WidgetTree()),
+                );
+              },
+              style: FilledButton.styleFrom(
+                minimumSize: Size(double.infinity, 40.0),
+              ),
+              child: Text('Get Started'),             
             ),
-            FilledButton(onPressed: () {
-              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const WidgetTree()));
-            }, child: Text('Login'))
+            SizedBox(height: 10.0,),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const WidgetTree()),
+                );
+              },
+              style: FilledButton.styleFrom(
+                minimumSize: Size(double.infinity, 40.0),
+              ),
+              child: Text('Login'),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+
+// from 2:46
